3af5d820474c978b72a717254a29bda0230379d8
Joao5498 <xibossaurorex@gmail.com>
2026-07-11 14:52:02 -0300

    Corrige estrutura do projeto e adiciona fontes Java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Tabuleiro.Posicao;
+
+public static void main(String[] args) {
+    Posicao p = new Posicao(3, 4);
+
+    System.out.println("Posicao " + p.ToString());
+
+}
---
+++ src/Tabuleiro/Posicao.java
@@ -0,0 +1,25 @@
+package Tabuleiro;
+
+public class Posicao {
+    public int coluna;
+    public int linha;
+
+    public Posicao(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    public String ToString(){
+        return linha + ", " + coluna;
+    }
+
+
+}
