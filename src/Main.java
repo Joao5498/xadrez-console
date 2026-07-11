de2b9a742ea1708018bb2d3d604c720a34826903
Joao5498 <xibossaurorex@gmail.com>
2026-07-11 16:00:09 -0300

    Tratando erros com excecao personalizada

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,28 @@ import tabuleiro.Cor;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import java.util.Scanner;
+import tabuleiro.TabuleiroException;
 import xadrez.*;
 
-public static void main(String[] args) {
+public static void main(String[] args) throws TabuleiroException {
     Scanner leitor = new Scanner(System.in);
+    try {
+        Tabuleiro tab = new Tabuleiro(8, 8);
 
-    Tabuleiro tab = new Tabuleiro(8,8);
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+        tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+        //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+        Tela.imprimirTabuleiro(tab);
+    }catch (TabuleiroException e){
+        System.out.println(e);
+    }
 
 
 
 
-    Tela.imprimirTabuleiro(tab);
+
 
     leitor.next();
 }
